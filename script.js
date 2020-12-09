1cd04797832a9b43ba46610c9f12213884278936
Ali.AlRubaye <ali_nasrat@hotmail.com>
2020-12-09 11:08:54 +0100

    add draw function

--- script.js
+++ script.js
@@ -11,21 +11,53 @@ class Game {
         this.stack = 21;
     };
 
-    draw(number){
-        this.stack = this.stack-number;
+    draw2(number) {
+        let current_player = this.players.forEach((x, index) => {
+            if (x.myTurn = true) {
+                x.stack = this.stack - number;
+                x.myTurn = false;
+            }
+
+        })
+        return current_player;
     };
 
-    addPlayer(playername){
+    addPlayer(playername) {
         let newPlayer = new Player(playername, false)
         this.players.push(newPlayer);
     };
+    get_player() {
+        if (this.stack == 21) {
+            this.players[0].myTurn = true;
+        }
+        return this.players;
+    }
+    draw(number) {
+        let currentplayer;
+        let theotherplayer;
+        for (let player of this.players) {
+            if (this.stack <= 0) {
+                return endgame
+            }
+            if (player.myTurn == true) {;
+                currentplayer = player;
+                this.stack = this.stack - number;
+                currentplayer.myTurn = false;
+            } else {
+                theotherplayer = player;
+                theotherplayer.myTurn = true;
+            }
 
+        }
+    }
 
 };
 
 let game1 = new Game()
 game1.addPlayer("Angelika");
 game1.addPlayer("Ali");
-game1.draw(2);
-
-console.log(game1);
+// game1.draw(2);
+console.log(game1.get_player());
+game1.draw(3)
+console.log(game1.stack)
+console.log(game1.get_player());
