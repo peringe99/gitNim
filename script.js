8b77c9b7d793d0e36ccdcb3cd6be7d75ffa4eef1
Gabriel Holmkvist <gabbeholman99@gmail.com>
2020-12-10 12:38:20 +0100

    fixed true winner and restart

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 class Player {
-    constructor(name, myTurn, score) {
+    constructor(name, myTurn) {
         this.name = name;
         this.myTurn = myTurn;
-        this.score = score;
+        this.score = 0;
     };
 };
 
@@ -10,52 +10,7 @@ class Game {
     constructor() {
         this.players = [];
         this.stack = 21;
-        this.throw();
-    };
-
-    throw () {
-        let game_sticks = document.getElementById('game_sticks');
-        game_sticks.innerHTML = '';
-        for (let i = 0; i < this.stack; i++) {
-            game_sticks.innerHTML += `<img src="../images/stick.png" id="stick_${i + 1}" alt="">`;
-        }
-    };
-
-    addPlayer(playername) {
-        let newPlayer = new Player(playername, false);
-        this.players.push(newPlayer);
-        this.players[0].myTurn = true;
-    };
-
-    get_player() {
-        return this.players;
-    };
-
-    check_player_turn(p) {
-        p.myTurn = p.myTurn ? false : true;
-    };
-
-    draw(number) {
-        for (let player of this.players) {
-            if (player.myTurn == true) {
-                this.stack -= number;
-                this.throw();
-                // console.log('player ' + player.name + ' ' + player.myTurn + ' ' + -number)
-                if (this.stack <= 0) {
-                    // console.log('you win ' + player.name + ' ' + player.myTurn)
-                    this.endGame(player)
-                }
-            } else {
-                // player.myTurn = true;
-                // this.check_player_turn(player)
-                // document.getElementById("player1ID").classList.add("active");
-            }
-            this.check_player_turn(player);
-        }
-        this.player_is_active()
-    };
-
-    startGame() {
+        // this.throw();
         let player1 = prompt("Name of player one?");
         let player2 = prompt("Name of player two?");
         this.addPlayer(player1);
@@ -70,6 +25,70 @@ class Game {
         this.player_is_active();
     };
 
+    throw () {
+        console.log("creating sticks1")
+        let game_sticks = document.getElementById('game_sticks');
+        game_sticks.innerHTML = '';
+        for (let i = 0; i < this.stack; i++) {
+            game_sticks.innerHTML += `<img src="../images/stick.png" id="stick_${i + 1}" alt="">`;
+        }
+    };
+
+    addPlayer(playername) {
+        let newPlayer = new Player(playername, false);
+        this.players.push(newPlayer);
+        this.players[0].myTurn = true;
+        console.log(this.players)
+    };
+
+    get_player() {
+        return this.players;
+    };
+
+    check_player_turn(p) {
+        p.myTurn = p.myTurn ? false : true;
+    };
+
+    draw(number) {
+        for (let i = 0; i < this.players.length; i++) {
+            if (this.players[i].myTurn == true) {
+                this.stack -= number;
+                this.throw();
+                // console.log('player ' + player.name + ' ' + player.myTurn + ' ' + -number)
+                if (this.stack <= 0) {
+                        console.log('you win ' + this.players[((i + 1) % 2)].name + ' ' + this.players[((i +1) % 2)].myTurn)
+                        this.endGame(this.players[((i + 1) % 2)])
+                        this.players[((i + 1) % 2)].score += 2
+                        console.log(this.players[((i + 1) % 2)].score)
+                }
+                
+            } else {
+                // player.myTurn = true;
+                // this.check_player_turn(player)
+                // document.getElementById("player1ID").classList.add("active");
+            }
+            this.check_player_turn(this.players[i]);
+        }
+        this.player_is_active()
+    };
+
+    startGame() {
+        this.stack = 21
+        this.throw()
+        // let player1 = prompt("Name of player one?");
+        // let player2 = prompt("Name of player two?");
+        // this.addPlayer(player1);
+        // this.addPlayer(player2);
+
+        // let player_one = document.getElementById("name_one");
+        // player_one.innerHTML = player1;
+        // player_one.parentElement.dataset.name = player1;
+        // let player_two = document.getElementById("name_two");
+        // player_two.innerHTML = player2;
+        // player_two.parentElement.dataset.name = player2;
+        // this.player_is_active();
+    };
+
     player_is_active() {
         let player_active = document.querySelectorAll('.player');
         player_active.forEach((x, index) => {
@@ -80,7 +99,7 @@ class Game {
         })
     };
     endGame(play) {
-        alert(`${play.name} har förlorat`)
+        alert(`${play.name} har vunnit`)
     }
 };
 
@@ -106,7 +125,7 @@ document.addEventListener("DOMContentLoaded", function(e) {
     let start_over = document.getElementById("btn_over");
 
     start_over.addEventListener("click", function() {
-        window.location.reload();
+        game1.startGame();
     })
 
     // highscoreBox.addEventListener("click", function(){
